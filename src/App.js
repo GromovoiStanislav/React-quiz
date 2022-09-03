d9b62aa6646a1f607e41f505443d3363d83683f4
Stanislav <gromila12@rambler.ru>
2022-09-03 10:41:11 +0600

    add new pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './hoc/Layout';
 import Quiz from './conteiners/Quiz';
+import Auth from './conteiners/Auth';
+import QuizCreator from './conteiners/QuizCreator';
+import QuizList from './conteiners/QuizList';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/quiz-creator" element={<QuizCreator />} />
+          <Route path="/quiz/:id" element={<Quiz />} />
+          <Route path="/" element={<QuizList />} />
+        </Routes>
       </Layout>
     );
   }
