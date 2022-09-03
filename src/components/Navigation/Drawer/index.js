c5d099a39425e1cec3ab6717e6326d2bdac5c0b4
Stanislav <gromila12@rambler.ru>
2022-09-03 10:58:54 +0600

    navigation

--- src/components/Navigation/Drawer/index.js
+++ src/components/Navigation/Drawer/index.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './style.module.css';
 import Backdrop from '../../UI/Backdrop';
 
-const links = [1, 2, 3];
+const links = [
+  { to: '/', label: 'Список' },
+  { to: '/auth', label: 'Авторизация' },
+  { to: '/quiz-creator', label: 'Создать тест' },
+];
 
 class Drawer extends Component {
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+            onClick={this.props.onClose}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
