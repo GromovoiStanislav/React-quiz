0a5fd2a23b0029ae7f99d5f0af854338ecf61cfc
Stanislav <gromila12@rambler.ru>
2022-09-05 10:12:07 +0600

    page Authorization

--- src/conteiners/Auth/index.js
+++ src/conteiners/Auth/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import styles from './style.module.css';
+import Button from '../../components/UI/Button';
 
 class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={styles.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler} className={styles.AuthForm}>
+            <input type="text" />
+            <input type="text" />
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
