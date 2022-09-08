c4b7b1402954ce5d855bd74059ca63c2bcc8fd65
Stanislav <gromila12@rambler.ru>
2022-09-08 11:49:07 +0600

    Authentication

--- src/conteiners/Auth/index.js
+++ src/conteiners/Auth/index.js
@@ -3,6 +3,7 @@ import is_js from 'is_js';
 import styles from './style.module.css';
 import Button from '../../components/UI/Button';
 import Input from '../../components/UI/Input';
+import axios from 'axios';
 
 class Auth extends Component {
   state = {
@@ -35,9 +36,39 @@ class Auth extends Component {
     },
   };
 
-  loginHandler = () => {};
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_API_KEY}`,
+        authData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  registerHandler = () => {};
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_API_KEY}`,
+        authData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   submitHandler = (event) => {
     event.preventDefault();
